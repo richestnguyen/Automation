4d42df0535ed32c9565547d850f8d1266eb05a68
Nguyen Phong Phu <richestnguyen@users.noreply.github.com>
2018-09-11 22:51:05 +0700

    added

---
+++ Automation/AutomationExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Automation.PageObjects;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+
+namespace Automation
+{
+    public static class AutomationExtensions
+    {
+        public static IWebDriver driver;
+        public static void GoToPage(this ScreenBase t, string url)
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+
+        public static void DoSearch(this HomePage t, string text)
+        {
+            Actions action  = new Actions(driver);
+            action.MoveToElement(t.SearhTextBox).Perform();
+            t.SearhTextBox.SendKeys(text);
+            Thread.Sleep(1000);
+            action.MoveToElement(t.SearchButton).Perform();
+            t.SearchButton.Click();
+        }
+
+        public static void ClickElement(IWebDriver driver, By element)
+        {
+            driver.FindElement(element).Click();
+        }
+
+        public static void SendKeys(IWebDriver driver, By element, string keys)
+        {
+            driver.FindElement(element).SendKeys(keys);
+        }
+
+        public static void SlowTypingValue(this IWebElement t, string text, int speed = 100)
+        {
+            foreach (var c in text)
+            {
+                t.SendKeys(c.ToString());
+                Thread.Sleep(speed);
+            }
+        }
+
+    }
+}
---
+++ Automation/CheckingExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Automation.PageObjects;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using Shouldly;
+
+namespace Automation
+{
+    public static class CheckingExtensions
+    {
+        public static void TitleBarShouldBe(this ScreenBase t, string screenUrl) 
+        {
+            t.driver.Title.ShouldBe(screenUrl);
+        } 
+
+        //public static ValidateElementIsPresent(IWebDriver driver, By element)
+        //{
+        //    driver.FindElement(element).Displayed.ShouldBe(true);
+        //}
+
+        //public static ValidateTextInElement(IWebDriver driver, By element, String text)
+        //{
+        //    driver.FindElement(element).Text.ShouldBe(text);
+        //} 
+    }
+}
